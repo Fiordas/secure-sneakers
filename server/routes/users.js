8c38d577d3ebe56a0b7ea911d537f1817f5d0c95
Simonas Juška <simonas.juska@outlook.com>
2018-05-22 11:16:02 +0200

    Add users route in backend

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/', (req, res) => {
+    var email = req.body.email;
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var password = req.body.password;
+    var confirmPassword = req.body.confirmPassword;
+    var userData = new User({
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+        password: password,
+        confirmPassword: confirmPassword
+    })
+
+    userData.save(function (error) {
+        if (error) {
+            console.log(error)
+        }
+        res.send({
+            success: true,
+            message: 'User data saved successfully!'
+        })
+    })
+})
 
 module.exports = router;
