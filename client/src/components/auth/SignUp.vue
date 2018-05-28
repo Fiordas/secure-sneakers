3988d4f1b383156097256ae17a3328301c397e90
andotri <triando.triando@gmail.com>
2018-05-28 17:50:21 +0200

    Update SignUp.vue
    - HTML validation
    - Password match

--- client/src/components/auth/SignUp.vue
+++ client/src/components/auth/SignUp.vue
@@ -2,6 +2,12 @@
   <div class="column is-4 is-offset-4">
     <div class="box" id="signupbox">
       <form @submit.prevent="onSubmit">
+        <p v-if="errors.length">
+          <b>Please correct the following error(s):</b>
+        <ul>
+          <li v-for="error in errors" :key="error.id">{{ error }}</li>
+        </ul>
+        </p>
         <div class="field">
           <label class="label">E-mail</label>
           <input
@@ -9,7 +15,7 @@
             type="email"
             id="email"
             placeholder="E-mail"
-            v-model="email">
+            v-model="email" required autofocus>
         </div>
         <div class="field">
           <label class="label">First Name</label>
@@ -18,7 +24,7 @@
             type="text"
             id="firstName"
             placeholder="First name"
-            v-model.number="firstName">
+            v-model.number="firstName" required pattern=".{3,}" title="3 characters minimum">
         </div>
         <div class="field">
           <label class="label">Last Name</label>
@@ -27,7 +33,7 @@
             type="text"
             id="lastName"
             placeholder="Last name"
-            v-model.number="lastName">
+            v-model.number="lastName" required pattern=".{3,}" title="3 characters minimum">
         </div>
         <div class="field">
           <label class="label">Password</label>
@@ -36,7 +42,7 @@
             type="password"
             id="password"
             placeholder="Password"
-            v-model="password">
+            v-model="password" required pattern=".{6,}" title="6 characters minimum">
         </div>
         <div class="field">
           <label class="label">Confirm Password</label>
@@ -44,8 +50,8 @@
             class="input"
             type="password"
             id="confirm-password"
-            placeholder="Repeat password"
-            v-model="confirmPassword">
+            placeholder="Confirm password"
+            v-model="confirmPassword" required pattern=".{6,}" title="6 characters minimum">
         </div>
         <div class="field submit">
           <button class="button is-primary" type="submit">Submit</button>
@@ -63,21 +69,21 @@ export default {
       email: '',
       firstName: '',
       lastName: '',
-      password: '',
-      confirmPassword: ''
+      errors: []
     }
   },
   methods: {
     onSubmit () {
-      /*
-      await UsersService.addUser({
-        email: this.email,
-        firstName: this.firstName,
-        lastName: this.lastName,
-        password: this.password,
-        confirmPassword: this.confirmPassword
-      })
-      */
+      // check error
+      this.errors = []
+      if (this.password !== this.confirmPassword) this.errors.push('Password does not match')
+      if (this.errors.length > 0) {
+        this.password = ''
+        this.confirmPassword = ''
+        return false
+      }
+
+      // post form
       const formData = {
         email: this.email,
         firstName: this.firstName,
@@ -86,6 +92,14 @@ export default {
       }
       console.log(formData)
       this.$store.dispatch('signUp', formData)
+
+      // set form to empty
+      this.email = ''
+      this.firstName = ''
+      this.lastName = ''
+      this.password = ''
+      this.confirmPassword = ''
+      alert('You have been registered, please sign in')
     }
   }
 }
