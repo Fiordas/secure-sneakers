cdcc3d831959fb29a395eea940b775315a7c8ca4
andotri <triando.triando@gmail.com>
2018-05-28 15:28:22 +0200

    Update Details.vue

--- client/src/components/Details.vue
+++ client/src/components/Details.vue
@@ -2,7 +2,7 @@
   <div>
     Size:
     <select>
-      <option v-model="selected" v-for="s in stock" v-if="s.quantity>0">{{s.size}}</option>
+      <option v-for="s in stock" :key="s.id" v-if="s.quantity>0">{{s.size}}</option>
     </select>
     <br>
     <button v-on:click="counter += 1">Add to chart</button>
@@ -12,33 +12,33 @@
 </template>
 
 <script>
-  import ProductsService from '@/services/ProductsService'
-  export default {
-    name: 'Details',
-    data () {
-      return {
-        id: '',
-        name: '',
-        brand: '',
-        description: '',
-        stock: [ ],
-        price: 0
-      }
-    },
-    mounted () {
-      this.getProduct()
-    },
-    methods: {
-      async getProduct () {
-        const response = await ProductsService.getProduct({
-          id: this.$route.params.id
-        })
-        this.name = response.data.name
-        this.brand = response.data.brand
-        this.description = response.data.description
-        this.stock = response.data.stock
-        this.price = response.data.price
-      },
+import ProductsService from '@/services/ProductsService'
+export default {
+  name: 'Details',
+  data () {
+    return {
+      id: '',
+      name: '',
+      brand: '',
+      description: '',
+      stock: [ ],
+      price: 0
+    }
+  },
+  mounted () {
+    this.getProduct()
+  },
+  methods: {
+    async getProduct () {
+      const response = await ProductsService.getProduct({
+        id: this.$route.params.id
+      })
+      this.name = response.data.name
+      this.brand = response.data.brand
+      this.description = response.data.description
+      this.stock = response.data.stock
+      this.price = response.data.price
     }
   }
+}
 </script>
