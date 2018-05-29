2bd5d3f473ba7e64b5eb4be95a6918f5d331d921
Unknown <imakarskaite@gmail.com>
2018-05-29 11:11:04 +0200

    Modal SignIn and SignUp. Clicking outside the modal closes it.

--- client/src/components/auth/SignUp.vue
+++ client/src/components/auth/SignUp.vue
@@ -1,71 +1,73 @@
 <template>
-  <div class="columns is-centered">
-    <div class="column is-4">
-      <div class="box is-radiusless">
-        <form @submit.prevent="onSubmit">
-          <p v-if="errors.length">
-            <b>Please correct the following error(s):</b>
-            <ul>
-              <li v-for="error in errors" :key="error.id">{{ error }}</li>
-            </ul>
-          </p>
-          <div class="field">
-            <label class="label">E-mail</label>
-            <input
-              class="input"
-              type="email"
-              placeholder="E-mail"
-              v-model="email" required autofocus>
-          </div>
-          <div class="field">
-            <label class="label">First Name</label>
-            <input
-              class="input"
-              type="text"
-              placeholder="First name"
-              v-model.number="firstName" required pattern=".{3,}" title="3 characters minimum">
-          </div>
-          <div class="field">
-            <label class="label">Last Name</label>
-            <input
-              class="input"
-              type="text"
-              placeholder="Last name"
-              v-model.number="lastName" required pattern=".{3,}" title="3 characters minimum">
-          </div>
-          <div class="field">
-            <label class="label">Password</label>
-            <vue-password v-model="password" classes="input" placeholder="Password" required>
-              <template slot="password-toggle" slot-scope="props">
-                <button class="VuePassword__Toggle"
-                        type="button"
-                        v-on:click = "props.toggle"
-                        v-text="props.type === 'password' ? 'SHOW' : 'HIDE'"
-                >
-                </button>
-              </template>
-            </vue-password>
-          </div>
-          <div class="field">
-            <label class="label">Confirm Password</label>
-            <vue-password v-model="confirmPassword" classes="input" placeholder="Confirm password" required>
-              <template slot="password-toggle" slot-scope="props">
-                <button class="VuePassword__Toggle"
-                        type="button"
-                        v-on:click = "props.toggle"
-                        v-text="props.type === 'password' ? 'SHOW' : 'HIDE'"
-                >
-                </button>
-              </template>
-            </vue-password>
-          </div>
-          <div class="field submit">
-            <button class="button is-primary" type="submit">Submit</button>
-          </div>
-        </form>
+  <div class="modal is-active">
+    <div class="modal-background" @click="$router.go(-1)"></div>
+      <div class="modal-content">
+        <div class="box is-radiusless">
+          <form @submit.prevent="onSubmit">
+            <p v-if="errors.length">
+              <b>Please correct the following error(s):</b>
+              <ul>
+                <li v-for="error in errors" :key="error.id">{{ error }}</li>
+              </ul>
+            </p>
+            <div class="field">
+              <label class="label">E-mail</label>
+              <input
+                class="input"
+                type="email"
+                placeholder="E-mail"
+                v-model="email" required autofocus>
+            </div>
+            <div class="field">
+              <label class="label">First Name</label>
+              <input
+                class="input"
+                type="text"
+                placeholder="First name"
+                v-model.number="firstName" required pattern=".{3,}" title="3 characters minimum">
+            </div>
+            <div class="field">
+              <label class="label">Last Name</label>
+              <input
+                class="input"
+                type="text"
+                placeholder="Last name"
+                v-model.number="lastName" required pattern=".{3,}" title="3 characters minimum">
+            </div>
+            <div class="field">
+              <label class="label">Password</label>
+              <vue-password v-model="password" classes="input" placeholder="Password" required>
+                <template slot="password-toggle" slot-scope="props">
+                  <button class="VuePassword__Toggle"
+                          type="button"
+                          v-on:click = "props.toggle"
+                          v-text="props.type === 'password' ? 'SHOW' : 'HIDE'"
+                  >
+                  </button>
+                </template>
+              </vue-password>
+            </div>
+            <div class="field">
+              <label class="label">Confirm Password</label>
+              <vue-password v-model="confirmPassword" classes="input" placeholder="Confirm password" required>
+                <template slot="password-toggle" slot-scope="props">
+                  <button class="VuePassword__Toggle"
+                          type="button"
+                          v-on:click = "props.toggle"
+                          v-text="props.type === 'password' ? 'SHOW' : 'HIDE'"
+                  >
+                  </button>
+                </template>
+              </vue-password>
+            </div>
+            <div class="field submit">
+              <button class="button is-primary" type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
       </div>
+      <button class="modal-close is-large" aria-label="close" @click="$router.go(-1)"></button>
     </div>
-  </div>
 </template>
 
 <script>
