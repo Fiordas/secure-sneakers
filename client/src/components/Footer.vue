88c438ef40bacb3c0cb304dc1e73b343b10fc890
buserylmz <buserylmz@gmail.com>
2018-05-31 06:00:32 -0700

    changed footer

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -13,7 +13,17 @@
 </template>
 
 <style>
-  footer {
-    margin-top: 30px;
+  .footer {
+    padding: 20px;
+    font-size: 12px;
+    text-align: right;
+    background: #000;
+    margin-top: 20px;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    color: #FFFFFF;
   }
+
 </style>
